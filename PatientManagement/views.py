c049abb3415da42f21c4a19f733db0c8be01554d
sujana26 <sujanasri2609@gmail.com>
2024-11-10 13:10:39 +0530

    added patient profile function

--- PatientManagement/views.py
+++ PatientManagement/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1 style='text-align:center;'>This is patient home</h1>")
+def profile(request):
+    return HttpResponse("<h1 style='text-align:center;'>This is patient profile</h1>")
